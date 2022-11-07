21d3a82dcf604b8015455a9e0db26c5854571090
bandlv <andriy.bobrovych@gmail.com>
2022-11-07 17:00:35 +0000

    add script

---
+++ install_apache.sh
@@ -0,0 +1,17 @@
+#/bin/bash
+
+USER_UID=$(echo $UID)
+OS_ID=$(cat /etc/os-release | grep "^ID=" | cut -d"=" -f2)
+
+if [[ $USER_UID -ne 0 ]];then
+	echo "you should run with su or sudo"
+	exit 1
+fi
+
+if [[ $OS_ID == "ubuntu" ]];then
+  apt update && apt -y install apache2
+  echo "<h1>Andriy Bobrovych</h1>" > /var/www/html/index.html
+elif [[ $OS_ID == "centos" ]];then
+  yum -y install httpd
+  echo "<h1>Andriy Bobrovych</h1>" > /usr/share/httpd/noindex/index.html
+fi 
